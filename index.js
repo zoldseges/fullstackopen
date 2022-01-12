e5b17088bd8c4c1ccb4f978ba03f9038c493077a
Palkovics Adam <pa.adam94@gmail.com>
2022-01-12 11:48:20 +0100

    part3.3

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json()) // for parsing
+const PORT=3001
+
+let persons = [
+    { 
+	"id": 1,
+	"name": "Arto Hellas", 
+	"number": "040-123456"
+    },
+    { 
+	"id": 2,
+	"name": "Ada Lovelace", 
+	"number": "39-44-5323523"
+    },
+    { 
+	"id": 3,
+	"name": "Dan Abramov", 
+	"number": "12-43-234345"
+    },
+    { 
+	"id": 4,
+	"name": "Mary Poppendieck", 
+	"number": "39-23-6423122"
+    }
+]
+
+app.get('/info', (request, response) => {
+    const date = new Date()
+    let len = persons.length
+    console.log(persons)
+    console.log(len)
+    response.send(`<p>Phonebook has ${len} people</p> <p>${date}</P`)
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+	response.json(person)
+    } else {
+	response.status(404).end()
+    }
+})
+
+
+app.listen(PORT, () => {
+    console
+	.log(`Server running on port ${PORT}`)
+})
+
