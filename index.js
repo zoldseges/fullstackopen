4ed40cc55d6de099bf382ea1ee9a3d74096deba9
Palkovics Adam <pa.adam94@gmail.com>
2022-01-12 12:42:15 +0100

    part3.6

--- index.js
+++ index.js
@@ -49,6 +49,47 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(note => note.id !== id)
+
+    response.status(204).end()
+})
+
+const generateId = () => 
+      Math.floor(Math.random() * 0x10000000)
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    
+    if (!body.name) {
+	return response.status(400).json({
+	    error: 'name missing'
+	})
+    }
+
+    if (persons.find(person => person.name.toLowerCase() === body.name.toLowerCase())) {
+	return response.status(400).json({
+	    error: 'name must be unique'
+	})
+    }
+
+    if (!body.number) {
+	return response.status(400).json({
+	    error: 'number missing'
+	})
+    }
+
+
+    const person = {
+	name: body.name,
+	number: body.number,
+	id: generateId()
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
+})
 
 app.listen(PORT, () => {
     console
