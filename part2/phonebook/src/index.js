e246cc626fe84cdd5c9a1874f67a1953cc671af9
palkovics adam <pa.adam94@gmail.com>
2022-01-16 13:24:09 +0000

    index.html added

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -1,4 +1,4 @@
-import useEffect from 'react'
+import React, {useEffect} from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.js'
 
--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = "http://localhost:3001/persons"
+const baseUrl = "http://localhost:3001/api/persons"
 
 const getAll = () => axios.get(baseUrl).then(response => response.data)
 const create = newPerson => axios.post(baseUrl, newPerson).then(response => response.data)
