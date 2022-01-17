58a1b8bc7eceb7bb41791829fa5845c5b4a46357
palkovics adam <pa.adam94@gmail.com>
2022-01-17 14:06:13 +0000

    3.15

---
+++ build/precache-manifest.97c248077b39bb098bb683984ce4676d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1097bc9314b29f87d3226eae8a20d96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56db7b282b7f648396b5",
+    "url": "/static/js/2.9e5c09c7.chunk.js"
+  },
+  {
+    "revision": "5b911cc2235b31b0de07",
+    "url": "/static/js/main.7a896848.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const Person = require('./models/person')
 
+app.use(express.static('build'))
 app.use(express.json()) // for parsing
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
-const PORT=3001
+const PORT=process.env.PORT
 
 morgan.token('content', (request, response) => {
     if (request.method === 'POST') {
@@ -15,55 +18,35 @@ morgan.token('content', (request, response) => {
     }
 })
 
-
-let persons = [
-    { 
-	"id": 1,
-	"name": "Arto Hellas", 
-	"number": "040-123456"
-    },
-    { 
-	"id": 2,
-	"name": "Ada Lovelace", 
-	"number": "39-44-5323523"
-    },
-    { 
-	"id": 3,
-	"name": "Dan Abramov", 
-	"number": "12-43-234345"
-    },
-    { 
-	"id": 4,
-	"name": "Mary Poppendieck", 
-	"number": "39-23-6423122"
-    }
-]
-
 app.get('/info', (request, response) => {
-    const date = new Date()
-    let len = persons.length
-    response.send(`<p>Phonebook has ${len} people</p> <p>${date}</P`)
+    Person.find({}).then(persons => {
+	const date = new Date()
+	response.send(`<p>Phonebook has ${persons.length} people</p> <p>${date}</P`)
+    })
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+	response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-	response.json(person)
-    } else {
-	response.status(404).end()
-    }
+    Person.findById(request.params.id).then(person => {
+	if (person) {
+	    response.json(person)
+	} else {
+	    response.status(404).end()
+	}
+    })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(note => note.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const generateId = () => 
@@ -77,12 +60,6 @@ app.post('/api/persons', (request, response) => {
 	})
     }
 
-    if (persons.find(person => person.name.toLowerCase() === body.name.toLowerCase())) {
-	return response.status(400).json({
-	    error: 'name must be unique'
-	})
-    }
-
     if (!body.number) {
 	return response.status(400).json({
 	    error: 'number missing'
@@ -90,18 +67,17 @@ app.post('/api/persons', (request, response) => {
     }
 
 
-    const person = {
+    const person = new Person({
 	name: body.name,
 	number: body.number,
-	id: generateId()
-    }
+    })
 
-    persons = persons.concat(person)
-    response.json(person)
+    person.save().then(savedPerson => {
+	response.json(savedPerson)
+    })
 })
 
 app.listen(PORT, () => {
-    console
-	.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
 
